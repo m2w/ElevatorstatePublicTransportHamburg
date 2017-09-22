a21e8de4ca2aeb85dee9a3224ed978a9dfde9425
René Kulik <info@renekulik.de>
2017-09-22 22:43:56 +0200

    Integrate dotenv

--- app/geofox.js
+++ app/geofox.js
@@ -1,5 +1,9 @@
 const axios = require('axios');
 const crypto = require('crypto');
+const dotenv = require('dotenv');
+
+// Load .env-files
+dotenv.load();
 
 let genSig = (body) => {
   let hmac = crypto.createHmac('sha1', process.env.GEOFOX_PSWD);
