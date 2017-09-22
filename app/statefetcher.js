94316011793f27a33ea6a22ee041aba3a6f87d34
Moritz <oss@tibidat.com>
2017-09-22 22:16:42 +0200

    Add loading of initial data to the Fetcher

--- app/statefetcher.js
+++ app/statefetcher.js
@@ -9,6 +9,7 @@ const EventEmitter = require('events');
 const GEOFOX_URL = 'https://geofox.hvv.de/data-service/rest/elevators/stations/';
 // private var
 let lastState = {};
+let cache = {};
 
 class Fetcher extends EventEmitter {
   constructor() {
@@ -18,6 +19,12 @@ class Fetcher extends EventEmitter {
     this.cron;
   };
   start() {
+    console.log('=== Initializing Elevator Status Monitor');
+    geofox.getAllStations().then((data) => {
+      console.log('=== Retrieved Station Data');
+      cache.stations = data;
+    });
+
     this._fetchStateAndGetDiff();
     this.cron = setInterval(
       ()=>this._fetchStateAndGetDiff(),
@@ -33,32 +40,32 @@ class Fetcher extends EventEmitter {
     const start = new Date().getTime();
     Axios(GEOFOX_URL).then(response => {
       if (response && response.status === 200) {
-        console.log(response.data);
         try {
           const json = response.data;
           var newState = {};
+          cache = json;
           // extracting hash of all states
-          json.stations.forEach(station =>{
-           Object.getOwnPropertyNames(station.elevators).forEach(elevatorId=>{
-            newState[station.gfxId + "_" + elevatorId] = station.elevators[elevatorId].state;
+          json.stations.forEach(station => {
+            Object.getOwnPropertyNames(station.elevators).forEach(elevatorId => {
+              newState[station.gfxId + "_" + elevatorId] = station.elevators[elevatorId].state;
             });
           });
-          let diffs = []; 
+          let diffs = [];
           // make diff
-          Object.getOwnPropertyNames(newState).forEach(elevatorId=>{
-          if (newState[elevatorId] != lastState[elevatorId]) {
-           let obj = {};
-           obj[elevatorId]= newState[elevatorId]
-            diffs.push(obj);
-          }
-        });         
-        this.emit('data', diffs);
-        // save new elevator state to state
-        Object.assign(this.lastState, newState);
-      }
-      catch(E) {
-        console.log(E);
-      }
+          Object.getOwnPropertyNames(newState).forEach(elevatorId => {
+            if (newState[elevatorId] != lastState[elevatorId]) {
+              let obj = {};
+              obj[elevatorId] = newState[elevatorId]
+              diffs.push(obj);
+            }
+          });
+          this.emit('data', diffs);
+          // save new elevator state to state
+          Object.assign(this.lastState, newState);
+        }
+        catch (E) {
+          console.log(E);
+        }
       } else {
         //console.log("Got an error: ", error, ", status code: ", response.statusCode)
       }
