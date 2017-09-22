29694259ee21ed6b622f2d36e4fcaa30ee567c6a
Moritz <oss@tibidat.com>
2017-09-22 22:00:42 +0200

    Fix more indentation issues

--- app/devicesdb.js
+++ app/devicesdb.js
@@ -3,51 +3,51 @@
 var sqlite3 = require('sqlite3').verbose();
 const DB = require('path').resolve(__dirname, "SubscribedDevices.sqlite");
 
-const onError= err => {
+const onError = err => {
 	if (err) {
 		return console.log(err.message);
 	}
 };
 
 module.exports = class {
-	constructor () { 
+	constructor() {
 		let db = new sqlite3.Database(DB);
-		db.run('CREATE TABLE IF NOT EXISTS devices(token text)');	
+		db.run('CREATE TABLE IF NOT EXISTS devices(token text)');
 		db.close(err => {
 			if (err) {
 				return console.log(err.message);
 			}
-		});	
+		});
 	};
-	
-	subscribe(token)  {
-    	let db = new sqlite3.Database(DB,sqlite3.OPEN_READWRITE);
-		db.run('INSERT INTO devices VALUES (?)',[token],onError);	    
+
+	subscribe(token) {
+		let db = new sqlite3.Database(DB, sqlite3.OPEN_READWRITE);
+		db.run('INSERT INTO devices VALUES (?)', [token], onError);
 		db.close();
 	};
-	
+
 	unsubscribe(token) {
-    	let db = new sqlite3.Database(DB,sqlite3.OPEN_READWRITE);
-		db.run('DELETE FROM devices WHERE token=?',token,onError);		
+		let db = new sqlite3.Database(DB, sqlite3.OPEN_READWRITE);
+		db.run('DELETE FROM devices WHERE token=?', token, onError);
 		db.close();
 	};
-	
+
 	getAll() {
 		var devices = [];
 		let db = new sqlite3.Database(DB);
 		return new Promise((resolve, reject) => {
 			db.all(`SELECT token FROM devices`, (err, rows) => {
 				if (err) {
-				  reject(err.message);
-			  }
-			  else {
-				 rows.forEach(row=>{
-					  devices.push(row.token);
-				 })
-			  }
-			  db.close();
-			  resolve(devices);
-		  });
-		});			
-	}	   	
+					reject(err.message);
+				}
+				else {
+					rows.forEach(row => {
+						devices.push(row.token);
+					})
+				}
+				db.close();
+				resolve(devices);
+			});
+		});
+	}
 };
